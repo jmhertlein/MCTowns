2b1f48526f55f2b78e36963522fbe2981d8bbe3a
Joshua M Hertlein <jmhertlein@gmail.com>
2013-03-22 00:25:27 -0500

    Finished polishing!

--- src/net/jmhertlein/mctowns/util/reporting/BugReportServerDaemon.java
+++ src/net/jmhertlein/mctowns/util/reporting/BugReportServerDaemon.java
@@ -2,7 +2,6 @@ package net.jmhertlein.mctowns.util.reporting;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.LinkedHashSet;
