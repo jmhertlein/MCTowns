0eb9479ea4335645d36ab9ac03fbfa6a54d0c482
Joshua M Hertlein <jmhertlein@gmail.com>
2014-01-04 04:19:05 -0600

    Cleaned up formatting...

--- src/main/java/net/jmhertlein/mctowns/command/executors/PlotExecutor.java
+++ src/main/java/net/jmhertlein/mctowns/command/executors/PlotExecutor.java
@@ -87,10 +87,11 @@ public class PlotExecutor extends BaseExecutor {
                     break;
                     
                     case "info":
-                    helpMessage = "/plot info";
-                    handler.printPlotInfo();
-                    softFailure = false;
-                    break;
+                        helpMessage = "/plot info";
+                        handler.printPlotInfo();
+                        softFailure = false;
+                        break;
+                        
                 case "list":
                 case "ls":
                     helpMessage = "/plot list players";
