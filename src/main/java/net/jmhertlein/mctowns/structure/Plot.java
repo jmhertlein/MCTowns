406da70bb02ee38084a87118a08914fc24452e67
Joshua M Hertlein <jmhertlein@gmail.com>
2014-01-15 21:25:28 -0600

    Fixed /plot info not handling names with _'s in them right

--- src/main/java/net/jmhertlein/mctowns/structure/Plot.java
+++ src/main/java/net/jmhertlein/mctowns/structure/Plot.java
@@ -112,12 +112,11 @@ public class Plot extends MCTownsRegion {
     public String getTerseName() {
         String absName = name;
 
-        while (absName.contains("_")) {
+        for(int i = 0; i < 2; i++) {
             absName = absName.substring(absName.indexOf('_') + 1);
         }
 
         return absName;
-
     }
 
     /**
