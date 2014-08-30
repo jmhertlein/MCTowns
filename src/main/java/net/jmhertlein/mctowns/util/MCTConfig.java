be292640880149d978cef96fe7b838bba4c202da
Joshua Michael Hertlein <jmhertlein@gmail.com>
2014-08-29 23:49:45 -0500

    Fixed license header again

--- src/main/java/net/jmhertlein/mctowns/util/MCTConfig.java
+++ src/main/java/net/jmhertlein/mctowns/util/MCTConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Joshua Michael Hertlein <jmhertlein@gmail.com>
+ * Copyright (C) 2014 Joshua Michael Hertlein <jmhertlein@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
