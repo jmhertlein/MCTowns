0e525d62bed5a42272d2a4b9d0f7aac356edfa70
Joshua Michael Hertlein <jmhertlein@gmail.com>
2015-12-23 11:51:52 -0600

    Made redefine availabe again

--- src/main/java/net/jmhertlein/mctowns/command/PlotHandler.java
+++ src/main/java/net/jmhertlein/mctowns/command/PlotHandler.java
@@ -367,4 +367,9 @@ public class PlotHandler extends CommandHandler implements CommandDefinition {
         }
         localSender.sendMessage("Active plot set to " + nuActive.getName());
     }
+
+    @CommandMethod(path = "plot redefine")
+    public void redefine() {
+        redefineActiveRegion(TownLevel.PLOT);
+    }
 }
